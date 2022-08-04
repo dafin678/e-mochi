dc6f6cb65556421f7d736e1eebab7e843403ac3f
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2022-08-04 13:07:01 +0700

    env configuration

--- app/server.go
+++ app/server.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
@@ -14,8 +16,14 @@ type Server struct {
 	Router *mux.Router
 }
 
-func (server *Server) Initialize() {
-	fmt.Println("Welcome To E-Mochi")
+type AppConfig struct {
+	AppName string
+	AppEnv  string
+	AppPort string
+}
+
+func (server *Server) Initialize(appConfig AppConfig) {
+	fmt.Println("Welcome To " + appConfig.AppName)
 	server.Router = mux.NewRouter()
 	server.initializeRoutes()
 }
@@ -25,8 +33,25 @@ func (server *Server) Run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, server.Router))
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
 func Run() {
 	var server = Server{}
-	server.Initialize()
-	server.Run(":9000")
+	var appConfig = AppConfig{}
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error on loading .env file")
+	}
+
+	appConfig.AppName = getEnv("APP_NAME", "EmochiStore")
+	appConfig.AppEnv = getEnv("APP_ENV", "development")
+	appConfig.AppPort = getEnv("APP_PORT", "9000")
+	server.Initialize(appConfig)
+	server.Run(":" + appConfig.AppPort)
 }
